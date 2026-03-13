13793a18f20d7b0ce7f52ec360fc2b6571ede7db
Helen <204595204+es-hp@users.noreply.github.com>
2026-03-13 06:29:51 -0400

    feat(overview): add banner with hero image
    Uses src from updated JSON to display hero image dynamically.

---
+++ src/js/components/overview/overviewBanner.js
@@ -0,0 +1,35 @@
+import { getTripDetails } from "../../core/api";
+import { createEl } from "../../core/utils";
+import countdownTimer from "./countdownTimer";
+
+export default async function overviewBanner(container) {
+  const tripDetails = await getTripDetails();
+  const site = tripDetails["site"];
+  const flagEmoji = tripDetails["flag"];
+  const year = new Date(tripDetails["departureDateTime"]).getFullYear();
+  const shortYear = "'" + String(year).slice(-2);
+
+  const subTitle = createEl("h2", { textContent: "Short Term Missions" });
+
+  const tripTitle = createEl("h1", {
+    textContent: `${flagEmoji} ${site} ${shortYear}`,
+  });
+
+  const description = createEl("p", {
+    textContent: tripDetails.description,
+    className: "banner-description",
+  });
+
+  const heroImgContainer = createEl("div", { className: "hero-img-container" });
+  const gradient = createEl("div", { className: "hero-gradient" });
+  const heroImg = createEl("img", {
+    src: tripDetails.heroImg,
+    alt: `Welcome to ${site} short term missions team.`,
+    className: "hero-img",
+  });
+
+  countdownTimer(tripDetails.departureDateTime);
+
+  heroImgContainer.append(heroImg, gradient);
+  container.append(subTitle, tripTitle, description, heroImgContainer);
+}
