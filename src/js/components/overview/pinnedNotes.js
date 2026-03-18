aeaee450b9d8ccc290e18809398e6bd9ac0f9583
Helen <204595204+es-hp@users.noreply.github.com>
2026-03-17 23:43:43 -0400

    refactor(pinnedNotes): use destructured object instead of separate params

--- src/js/components/overview/pinnedNotes.js
+++ src/js/components/overview/pinnedNotes.js
@@ -1,7 +1,7 @@
 import { createEl } from "../../core/utils";
 import createTile from "../design-system/createTile";
 
-export default function pinnedNotes(container, tripDetails) {
+export default function pinnedNotes({ container, tripDetails }) {
   const content = createEl("div", { className: "pinned-content" });
   const pin = createEl("div", { className: "pin", textContent: "📌" });
 
