4d9c652a7f3fea868fbb0e09004c2d68ee2009fd
Helen <204595204+es-hp@users.noreply.github.com>
2026-03-27 23:57:40 -0400

    WIP: prayer request card structure

--- src/js/components/profile/userPrayers.js
+++ src/js/components/profile/userPrayers.js
@@ -1,9 +1,16 @@
-import { createEl, createCardIcon, formatDateTime, getCSSVar } from "@utils";
+import {
+  createEl,
+  createLucideIcon,
+  createCardIcon,
+  formatDateTime,
+  getCSSVar,
+} from "@utils";
 import createTile from "../design-system/createTile";
 import { getUserPosts } from "@/js/core/api";
 
 const openColor = getCSSVar("--color-text");
 const closedColor = getCSSVar("--color-text-card-muted");
+const urgentIconColor = getCSSVar("--color-red");
 
 export default async function userPrayers({ container, user }) {
   const userId = user.id;
@@ -14,6 +21,8 @@ export default async function userPrayers({ container, user }) {
     closed: { text: "Closed", color: closedColor },
   };
   const body = [];
+
+  const closedPosts = createEl("div", { className: "closed-posts" });
 
   posts.forEach((post) => {
     const postCard = createEl("article", {
@@ -43,9 +52,47 @@ export default async function userPrayers({ container, user }) {
     metadata.append(status, timestamp);
     header.append(icon, metadata);
 
+    if (post.isUrgent) {
+      const urgentIcon = createLucideIcon("ClockAlert", {
+        size: "1.25rem",
+        strokeWidth: 2,
+        color: urgentIconColor,
+      });
+      urgentIcon.style.marginLeft = "auto";
+      urgentIcon.style.alignSelf = "flex-start";
+      header.append(urgentIcon);
+    }
+
     /* Post Body */
     const postBody = createEl("div", { className: "prayer-request-body" });
-    postBody.textContent = post.content;
+    const postContent = createEl("p", {
+      className: "post-initial-content",
+      textContent: post.content,
+    });
+
+    postBody.append(postContent);
+
+    /* Updates */
+    if (post.updates) {
+      post.updates.forEach((update) => {
+        const divider = createEl("hr", { className: "post-divider" });
+        const updatePost = createEl("div", { className: "post-update" });
+        const { date, time, zdtAttribute } = formatDateTime(update.createdAt, {
+          month: "short",
+        });
+        const updateTimestamp = createEl("time", {
+          dateTime: zdtAttribute,
+          className: "post-timestamp",
+          textContent: `Posted ${date} at ${time}`,
+        });
+        const updateContent = createEl("p", {
+          className: "post-update-content",
+          textContent: update.content,
+        });
+        updatePost.append(updateTimestamp, updateContent);
+        postBody.append(divider, updatePost);
+      });
+    }
 
     /* Post Footer */
     const prayedCountForm = createEl("form", {
@@ -57,7 +104,7 @@ export default async function userPrayers({ container, user }) {
       className: "prayed-count-text",
       textContent: "test text: Prayed for # times.",
     });
-    const submitBtn = createEl("a", {
+    const submitBtn = createEl("button", {
       href: "#",
       className: "prayed-submit-btn btn primary-btn",
       textContent: "Prayed for this",
@@ -70,8 +117,23 @@ export default async function userPrayers({ container, user }) {
     prayedCountForm.append(submitCount, submitBtn, inputPostId);
 
     postCard.append(header, postBody, prayedCountForm);
-    body.push(postCard);
+
+    if (post.status === "closed") {
+      submitBtn.disabled = true;
+      closedPosts.append(postCard);
+    } else {
+      body.push(postCard);
+    }
   });
+
+  const closedPostsContainer = createEl("details");
+  const closedPostsToggle = createEl("summary", {
+    className: "closed-posts-toggle",
+    textContent: "See previous prayer requests",
+  });
+
+  closedPostsContainer.append(closedPostsToggle, closedPosts);
+  body.push(closedPostsContainer);
 
   container.classList.add("tile");
 
