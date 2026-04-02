f50206376546688de5c382c03d9e2959b8007c3f
Helen <204595204+es-hp@users.noreply.github.com>
2026-04-02 11:10:33 -0400

    chore(json): add user prayer requests mock data

--- src/js/components/profile/userRoles.js
+++ src/js/components/profile/userRoles.js
@@ -3,12 +3,13 @@ import { authCheck } from "@core/auth";
 import createTile from "../design-system/createTile";
 
 export default async function userRoles({ container, profileUser }) {
-  const { isOwner, currentUser } = await authCheck(profileUser.id);
+  // const { isOwner, currentUser } = await authCheck(profileUser.id);
   const roles = profileUser.logistics.roles;
 
-  const header = "Roles and Responsibilities";
+  const header = "Roles";
 
   const rolesList = createEl("ul", { className: "profile-roles-list" });
+  rolesList.style.textTransform = "Capitalize";
 
   if (roles) {
     roles.forEach((role) => {
@@ -17,8 +18,10 @@ export default async function userRoles({ container, profileUser }) {
     });
   }
 
+  const divider = createEl("hr", { className: "post-divider" });
+
   const devos = createEl("div", { className: "profile-devos" });
-  const devoAssignmentTitle = createEl("h3", {
+  const devoAssignmentTitle = createEl("h2", {
     textContent: "Leading Team Devo",
   });
   const assignmentDay = createEl("p", {
@@ -26,7 +29,7 @@ export default async function userRoles({ container, profileUser }) {
   });
   devos.append(devoAssignmentTitle, assignmentDay);
 
-  const body = [rolesList, devos];
+  const body = [rolesList, divider, devos];
 
   createTile({ container, header, body });
 }
